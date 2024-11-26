68dd5f3c4bb96fc7de57c95177634aeece10a0e9
Sanawar_21 <sanawarsaeed@gmail.com>
2024-11-26 20:54:01 +0500

    change build file

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
-gcc -I/usr/include/libnotify -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include \
-    -I/usr/include/gdk-pixbuf-2.0 -I/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0/include \
-    src/main.c src/alerts/alerts.c src/reports/reports.c src/http/http_request.c src/render/render.c \
-    -lcurl -lnotify -lgobject-2.0 -lglib-2.0 -o build/main
+gcc src/main.c src/alerts/alerts.c src/reports/reports.c src/http/http_request.c src/render/render.c \
+    -lcurl -o build/main
 
-gcc -I/usr/include/libnotify -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include \
-    -I/usr/include/gdk-pixbuf-2.0 -I/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0/include \
-    src/notifier.c src/alerts/alerts.c src/reports/reports.c src/http/http_request.c src/render/render.c \
-    -lcurl -lnotify -lgobject-2.0 -lglib-2.0 -o build/notifier
+gcc src/notifier.c src/alerts/alerts.c src/reports/reports.c src/http/http_request.c src/render/render.c \
+    -lcurl -o build/notifier
