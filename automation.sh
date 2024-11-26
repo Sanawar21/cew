5284bed5caf27b922c5428313985f63327ff2f00
Sanawar_21 <sanawarsaeed@gmail.com>
2024-11-26 20:54:45 +0500

    change automation.sh

--- automation.sh
+++ automation.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-
-# Define the cron job command
-CRON_JOB="* * * * * build/notifier >> logs/output.log 2>&1"
-
-# Check if the cron job is already present
-crontab -l | grep -q "build/notifier"
-if [ $? -eq 0 ]; then
-    echo "Cron job already exists. No changes made."
-else
-    # Add the cron job
-    (crontab -l; echo "$CRON_JOB") | crontab -
-    echo "Cron job added."
-fi
+./build/notifier
